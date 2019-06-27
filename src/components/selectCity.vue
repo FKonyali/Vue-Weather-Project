58c2e5a2fc754a9d81ba4f2f753edd74771c39ae
Furkan Konyali <furkan.konyali@gmail.com>
2019-06-27 18:01:50 +0300

    guncelleme yapildi.

--- src/components/selectCity.vue
+++ src/components/selectCity.vue
@@ -1,100 +1,69 @@
 <template>
     <div class="select">
-        <div class="select__item select__item--is-selected" v-on:click="clickCity">
+        <div class="select__item select__item--is-selected" v-on:click="openAllCity">
             {{geoFindLocation}}
         </div>
-        <div class="select__item" v-for="item in cities" :key="item.id">
-            {{item.il}}
+        <div class="select__contents" v-if="openAllCityOpen === true">
+            <div class="select__item" v-for="item in cities" :key="item.id">
+                <span v-on:click="clickCity(item.merkezId)" v-html="item.il">{{item.il}}</span>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+import { async } from 'q';
 export default {
     name: 'selectCity',
-    props: ['deneme'],
     data() {
         return {
             selectedCity: '',
-            geoGetLat: '',
-            geoGetLon: '',
             clickAble: true,
             weahterShort: '',
             resultTemplateArr: [],
             order: '',
             cities: [],
-            geoFindLocation: ''
+            geoFindLocation: '',
+            geoButtonsGet: '',
+            coordinator: [],
+            selectedMerkezID: '',
+            openAllCityOpen: false
         }
     },
-    created() {
+    async created() {
         let _this = this;
-        //getGeo
-        function getLocation() {
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(showPosition);
-            } else { 
-                //console.log("Tarayıcın geolocation desteklemiyor knk");
-            }
-        }
-
-        getLocation();
-
-        function showPosition(position) {
-            console.log('...');
-           _this.geoGetLat = position.coords.latitude;
-           _this.geoGetLon = position.coords.longitude;
-
-            console.log(_this.cities);
-            for(let a = 0; a < _this.cities; a++) {
-                if(distance(_this.geoGetLon, _this.geoGetLat, _this.cities[a].boylam, _this.cities[a].enlem) > 0 && distance(_this.geoGetLon, _this.geoGetLat, _this.cities[a].boylam, _this.cities[a].enlem) < 30) {
-                    _this.geoFindLocation = _this.cities[a].il;
-                }
-            }
-        }
-
-        function distance(lon1, lat1, lon2, lat2) {
-            var R = 6371; // Radius of the earth in km
-            var dLat = (lat2-lat1).toRad();  // Javascript functions in radians
-            var dLon = (lon2-lon1).toRad(); 
-            var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-                    Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) * 
-                    Math.sin(dLon/2) * Math.sin(dLon/2); 
-            var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-            var d = R * c; // Distance in km
-            return d;
-        }
-
-        if (typeof(Number.prototype.toRad) === "undefined") {
-            Number.prototype.toRad = function() {
-                return this * Math.PI / 180;
-            }
-        }
-
-        fetch('https://servis.mgm.gov.tr/api/merkezler/iller')
+        await fetch('https://servis.mgm.gov.tr/api/merkezler/iller')
             .then(res => res.json())
             .then(data => {
                 this.order = data.sort((a,b) => (a.il > b.il) ? 1 : ((b.il > a.il) ? -1 : 0));
-                $route.query.cities = this.order;
+                this.cities = this.order;
 
-                console.log(this.cities);
-
-                for(let a = 0; a < data.length; a++) {
-                    if(distance(this.geoGetLon, this.geoGetLat, data[a].boylam, data[a].enlem) > 0 && distance(this.geoGetLon, this.geoGetLat, data[a].boylam, data[a].enlem) < 30) {
-                        this.geoFindLocation = data[a].il;
+                //getGeo
+                console.log(this.geoButtonsGet);
+                
+                if(this.geoButtonsGet == 'allow') {
+                    console.log('...');
+                    for(let a = 0; a < data.length; a++) {
+                        //console.log(distance(this.coordinator[0].x, this.coordinator[0].y, data[a].boylam, data[a].enlem));
+                        if(this.distance(this.coordinator[0].y, this.coordinator[0].x, data[a].boylam, data[a].enlem) > 0 && this.distance(this.coordinator[0].y, this.coordinator[0].x, data[a].boylam, data[a].enlem) < 30) {
+                            this.geoFindLocation = data[a].il;
+                            this.selectedMerkezID = data[a].merkezId;
+                            this.clickCity(this.selectedMerkezID);
+                        }
                     }
                 }
-            })
-
-        console.log(this.cities);
+            });
     },
     methods: {
         clickCity: function(e) {
-            if(this.clickAble == true) {
-                this.clickAble = false;
+            if(this.openAllCityOpen == true) {
+                this.geoFindLocation = 'Adana';
+            }
+            this.openAllCityOpen = false;
+            
                 fetch('https://servis.mgm.gov.tr/api/sondurumlar?merkezid='+e)
                     .then(res => res.json())
                     .then(data => {
-                    this.weatherResult = true;
                     this.resultTemplateArr = data;
 
                     switch(data[0].hadiseKodu){
@@ -139,8 +108,61 @@ export default {
                             break;
                     }
                 })
+        },
+        getLocation: function() {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(this.allowGeo, this.denyGeo);
+            } else { 
+                console.log("Tarayıcın geolocation desteklemiyor knk");
+            }
+        },
+        allowGeo(position) {
+            console.log(this.coordinator);
+            this.geoButtonsGet = 'allow';
+            this.coordinator = [
+                {
+                    x: position.coords.latitude,
+                    y: position.coords.longitude
+                }
+            ]
+            for(let a = 0; a < this.cities.length; a++) {
+                if(this.distance(this.coordinator[0].y, this.coordinator[0].x, this.cities[a].boylam, this.cities[a].enlem) > 0 && this.distance(this.coordinator[0].y, this.coordinator[0].x, this.cities[a].boylam, this.cities[a].enlem) < 30) {
+                    this.geoFindLocation = this.cities[a].il;
+                    this.selectedMerkezID = this.cities[a].merkezId;
+                    this.clickCity(this.selectedMerkezID);
+                }
+            }
+        },
+        denyGeo(err) {
+            this.geoButtonsGet = 'deny';
+            console.log('deny neden yaptın knk');
+        },
+        distance(lon1, lat1, lon2, lat2) {
+            if (typeof(Number.prototype.toRad) === "undefined") {
+                Number.prototype.toRad = function() {
+                    return this * Math.PI / 180;
+                }
+            }
+            var R = 6371; // Radius of the earth in km
+            var dLat = (lat2-lat1).toRad();  // Javascript functions in radians
+            var dLon = (lon2-lon1).toRad(); 
+            var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+                    Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) * 
+                    Math.sin(dLon/2) * Math.sin(dLon/2); 
+            var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+            var d = R * c; // Distance in km
+            return d;
+        },
+        openAllCity: function() {
+            if(this.openAllCityOpen == false) {
+                this.openAllCityOpen = true;
+            }else {
+                this.openAllCityOpen = false;
             }
         }
+    },
+    beforeMount() {
+        this.getLocation();
     }
 }
 </script>
