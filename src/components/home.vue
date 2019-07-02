04bc145c0094e89184e13868cde1f40dd34e33c0
Furkan Konyali <furkan.konyali@gmail.com>
2019-07-02 15:29:55 +0300

    ilceler ve input autocomplate eklendi.

--- src/components/home.vue
+++ src/components/home.vue
@@ -12,8 +12,31 @@
                 {{geoFindLocation}}
               </div>
               <div class="select__contents" v-show="openAllCityOpen === true">
-                <div class="select__item" v-for="item in cities" :key="item.id">
-                  <span v-on:click="clickCity(item.merkezId, $event)">{{item.il}}</span>
+                <div class="select__search">
+                  <input class="select__input" placeholder="Şehir ara..." v-model="cfilter">
+                </div>
+                <div class="select__scroll">
+                  <div class="select__item" v-for="item in citiesFilter" :key="item.id">
+                    <span v-on:click="clickCity(item.merkezId, $event)">{{item.il}}</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="select__hr">
+              -
+            </div>
+            <div class="select">
+              <div class="select__item select__item--is-selected" v-on:click="openAllCityDistrictClick">
+                {{geoFindLocationDistrict}}
+              </div>
+              <div class="select__contents" v-show="openAllCityDistrict === true">
+                <div class="select__search">
+                  <input type="text" class="select__input" placeholder="Yer ara..." v-model="dfilter">
+                </div>
+                <div class="select__scroll">
+                  <div class="select__item" v-for="item in citiesDistrictFilter" :key="item.id">
+                    <span v-on:click="clickCityDistrict(item.merkezId, $event)">{{item.ilce}}</span>
+                  </div>
                 </div>
               </div>
             </div>
@@ -62,6 +85,7 @@
 </template>
 
 <script>
+import { async } from 'q';
 export default {
     name: 'selectCity',
     data() {
@@ -71,7 +95,9 @@ export default {
             weahterShort: '',
             resultTemplateArr: [],
             order: '',
+            orderDistrict: '',
             cities: [],
+            citiesDistrict: [],
             geoFindLocation: '',
             geoButtonsGet: '',
             coordinator: [],
@@ -79,153 +105,73 @@ export default {
             openAllCityOpen: false,
             fiveDayWeather: '',
             time: '',
-            topOffset: ''
+            topOffset: '',
+            geoFindLocationDistrict: '',
+            openAllCityDistrict: false,
+            searchCity: '',
+            cfilter: '',
+            dfilter: ''
         }
     },
     async created() {
-        await fetch('https://servis.mgm.gov.tr/api/merkezler/iller')
-            .then(res => res.json())
-            .then(data => {
-                this.order = data.sort((a,b) => (a.il > b.il) ? 1 : ((b.il > a.il) ? -1 : 0));
-                this.cities = this.order;
+      await fetch('https://servis.mgm.gov.tr/api/merkezler/iller')
+        .then(res => res.json())
+        .then(data => {
+            this.order = data.sort((a,b) => (a.il > b.il) ? 1 : ((b.il > a.il) ? -1 : 0));
+            this.cities = this.order;
 
-                //getGeo
-                //console.log(this.geoButtonsGet);
-                
-                if(this.geoButtonsGet == 'allow') {
-                    //console.log('...');
-                    for(let a = 0; a < data.length; a++) {
-                        //console.log(distance(this.coordinator[0].x, this.coordinator[0].y, data[a].boylam, data[a].enlem));
-                        //console.log(this.distance(this.coordinator[0].y, this.coordinator[0].x, data[a].boylam, data[a].enlem));
-                        if(this.distance(this.coordinator[0].y, this.coordinator[0].x, data[a].boylam, data[a].enlem) > 0 && this.distance(this.coordinator[0].y, this.coordinator[0].x, data[a].boylam, data[a].enlem) < 30) {
-                            this.geoFindLocation = data[a].il;
-                            this.selectedMerkezID = data[a].merkezId;
-                            this.clickCity(this.selectedMerkezID);
-                        }
+            //getGeo
+            //console.log(this.geoButtonsGet);
+            
+            if(this.geoButtonsGet == 'allow') {
+                //console.log('...');
+                for(let a = 0; a < data.length; a++) {
+                    //console.log(distance(this.coordinator[0].x, this.coordinator[0].y, data[a].boylam, data[a].enlem));
+                    //console.log(this.distance(this.coordinator[0].y, this.coordinator[0].x, data[a].boylam, data[a].enlem));
+                    if(this.distance(this.coordinator[0].y, this.coordinator[0].x, data[a].boylam, data[a].enlem) > 0 && this.distance(this.coordinator[0].y, this.coordinator[0].x, data[a].boylam, data[a].enlem) < 30) {
+                        this.geoFindLocation = data[a].il;
+                        this.selectedMerkezID = data[a].merkezId;
+                        this.geoFindLocationDistrict = data[a].ilce;
+                        this.clickCity(this.selectedMerkezID);
                     }
                 }
-            });
+            }
+        });
+        
     },
     methods: {
         clickCity: function(e,x) {
-            if(this.openAllCityOpen == true) {
-                this.geoFindLocation = x.currentTarget.innerHTML;
-                this.topOffset = x.currentTarget.offsetTop;
-            }
-            this.openAllCityOpen = false;
+          if(this.openAllCityOpen == true) {
+            this.geoFindLocation = x.currentTarget.innerHTML;
+            this.topOffset = x.currentTarget.offsetTop;
+          }
+          this.openAllCityOpen = false;
 
-            
-            
-                fetch('https://servis.mgm.gov.tr/api/sondurumlar?merkezid='+e)
-                    .then(res => res.json())
-                    .then(data => {
-                    let MM = ["Ocak", "Şubat","Mart","Nisan","Mayıs","Haziran","Temmuz","Ağustos","Eylül","Ekim","Kasım", "Aralık"],
-                        
-                        newDate = new Date(data[0].veriZamani),
-                        newdateDay = newDate.getDate(),
-                        newdateMount = MM[newDate.getMonth()],
-                        newdateClock = newDate.getHours() + '.' + newDate.getMinutes();
-                      
-                    this.resultTemplateArr = {
-                                              degree: data[0].sicaklik.toFixed(0) + '°',
-                                              icon: data[0].hadiseKodu,
-                                              date: newdateDay + ' ' + newdateMount + ' - ' + newdateClock,
-                                              wind: data[0].ruzgarHiz.toFixed(0) + 'km/sa',
-                                            };
+          this.fetchMerkezId(e);
+          this.fetchIlceId(this.geoFindLocation);
 
+          //reset input
+          this.cfilter = '';
+          this.dfilter = '';
+        },
+        clickCityDistrict: function(e, x) {
+          if(this.openAllCityDistrict == true) {
+            this.geoFindLocationDistrict = x.currentTarget.innerHTML;
+            this.topOffset = x.currentTarget.offsetTop;
+          }
+          this.openAllCityDistrict = false;
 
-                    switch(data[0].hadiseKodu){
-                        case 'K':
-                            this.weahterShort = 'Karlı';
-                            break;
-                        case 'A':
-                            this.weahterShort = 'Açık';
-                            break;
-                        case 'PB':
-                            this.weahterShort = 'Parçalı Bulutlu';
-                            break;
-                        case 'AB':
-                            this.weahterShort = 'Az Bulutlu';
-                            break;
-                        case 'CB':
-                            this.weahterShort = 'Çok Bulutlu';
-                            break;
-                        case 'HSY':
-                            this.weahterShort = 'hsy';
-                            break;
-                        case 'GSY':
-                            this.weahterShort = 'Gök Gürültülü Sağnak Yağışlı';
-                            break;
-                        case 'KKY':
-                            this.weahterShort = 'Karla Karışık Yağmur';
-                            break;
-                        case 'MSY':
-                            this.weahterShort = 'msy';
-                            break;
-                        case 'PUS':
-                            this.weahterShort = 'pus';
-                            break;
-                        case 'SY':
-                            this.weahterShort = 'Sağnak Yağışlı';
-                            break;
-                        case 'Y':
-                            this.weahterShort = 'Yağışlı';
-                            break;
-                        default:
-                            this.weahterShort = '';
-                            break;
-                    }
-                }).then(() => {
-                  let dd = ['Pzt', 'Sal', 'Çar', 'Per', 'Cum', 'Cmt', 'Paz'];
-                  fetch('https://servis.mgm.gov.tr/api/tahminler/gunluk?istno='+e)
-                      .then(resG => resG.json())
-                      .then(dataG => {
-                        let newGDate1 = new Date(dataG[0].tarihGun1),
-                            newGdateMount1 = dd[newGDate1.getDay()],
-                            newGDate2 = new Date(dataG[0].tarihGun2),
-                            newGdateMount2 = dd[newGDate2.getDay()],
-                            newGDate3 = new Date(dataG[0].tarihGun3),
-                            newGdateMount3 = dd[newGDate3.getDay()],
-                            newGDate4 = new Date(dataG[0].tarihGun4),
-                            newGdateMount4 = dd[newGDate4.getDay()],
-                            newGDate5 = new Date(dataG[0].tarihGun5),
-                            newGdateMount5 = dd[newGDate5.getDay()];
-                        this.fiveDayWeather = [
-                          {
-                            degree: ((dataG[0].enDusukGun1 + dataG[0].enYuksekGun1) / 2).toFixed(0) + '°',
-                            icon: dataG[0].hadiseGun1,
-                            date: newGdateMount1
-                          },
-                          {
-                            degree: ((dataG[0].enDusukGun2 + dataG[0].enYuksekGun2) / 2).toFixed(0) + '°',
-                            icon: dataG[0].hadiseGun2,
-                            date: newGdateMount2
-                          },
-                          {
-                            degree: ((dataG[0].enDusukGun3 + dataG[0].enYuksekGun3) / 2).toFixed(0) + '°',
-                            icon: dataG[0].hadiseGun3,
-                            date: newGdateMount3
-                          },
-                          {
-                            degree: ((dataG[0].enDusukGun4 + dataG[0].enYuksekGun4) / 2).toFixed(0) + '°',
-                            icon: dataG[0].hadiseGun4,
-                            date: newGdateMount4
-                          },
-                          {
-                            degree: ((dataG[0].enDusukGun5 + dataG[0].enYuksekGun5) / 2).toFixed(0) + '°',
-                            icon: dataG[0].hadiseGun5,
-                            date: newGdateMount5
-                          }
-                        ]
-                        //console.log(this.fiveDayWeather);
-                      });
-                })
+          this.fetchMerkezId(e);
+
+          //reset input
+          this.cfilter = '';
+          this.dfilter = '';
         },
         getLocation: function() {
             if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(this.allowGeo, this.denyGeo);
+              navigator.geolocation.getCurrentPosition(this.allowGeo, this.denyGeo);
             } else { 
-                //console.log("Tarayıcın geolocation desteklemiyor knk");
+              //console.log("Tarayıcın geolocation desteklemiyor knk");
             }
         },
         allowGeo(position) {
@@ -241,6 +187,7 @@ export default {
                 if(this.distance(this.coordinator[0].y, this.coordinator[0].x, this.cities[a].boylam, this.cities[a].enlem) > 0 && this.distance(this.coordinator[0].y, this.coordinator[0].x, this.cities[a].boylam, this.cities[a].enlem) < 30) {
                     this.geoFindLocation = this.cities[a].il;
                     this.selectedMerkezID = this.cities[a].merkezId;
+                    this.geoFindLocationDistrict = data[a].ilce;
                     this.clickCity(this.selectedMerkezID);
                 }
             }
@@ -270,12 +217,103 @@ export default {
             return d;
         },
         openAllCity: function() {
-            if(this.openAllCityOpen == false) {
-                this.openAllCityOpen = true;
-            }else {
-                this.openAllCityOpen = false;
+          if(this.openAllCityOpen == false) {
+              this.openAllCityOpen = true;
+              this.openAllCityDistrict = false;
+          }else {
+              this.openAllCityOpen = false;
+          }
+        },
+        openAllCityDistrictClick: function() {
+          if(this.openAllCityDistrict == false) {
+            this.openAllCityDistrict = true;
+            this.openAllCityOpen = false;
+          }else {
+            this.openAllCityDistrict = false;
+          }
+        },
+        fetchMerkezId: function(e) {
+          fetch('https://servis.mgm.gov.tr/api/sondurumlar?merkezid='+e)
+            .then(res => res.json())
+            .then(data => {
+            let MM = ["Ocak", "Şubat","Mart","Nisan","Mayıs","Haziran","Temmuz","Ağustos","Eylül","Ekim","Kasım", "Aralık"],
+                
+            newDate = new Date(data[0].veriZamani),
+            newdateDay = newDate.getDate(),
+            newdateMount = MM[newDate.getMonth()],
+            newdateClock = newDate.getHours() + '.' + newDate.getMinutes();
+              
+            this.resultTemplateArr = {
+                                      degree: data[0].sicaklik.toFixed(0) + '°',
+                                      icon: data[0].hadiseKodu,
+                                      date: newdateDay + ' ' + newdateMount + ' - ' + newdateClock,
+                                      wind: data[0].ruzgarHiz.toFixed(0) + 'km/sa',
+                                    };
+
+            for(let a = 0; a < this.cities.length; a++) {
+              if(e == this.cities[a].merkezId){
+                this.geoFindLocationDistrict = this.cities[a].ilce;
+              }
             }
-            
+
+          }).then(() => {
+            let dd = ['Pzt', 'Sal', 'Çar', 'Per', 'Cum', 'Cmt', 'Paz'];
+            fetch('https://servis.mgm.gov.tr/api/tahminler/gunluk?istno='+e)
+                .then(resG => resG.json())
+                .then(dataG => {
+                  try {
+                    let newGDate1 = new Date(dataG[0].tarihGun1),
+                      newGdateMount1 = dd[newGDate1.getDay()],
+                      newGDate2 = new Date(dataG[0].tarihGun2),
+                      newGdateMount2 = dd[newGDate2.getDay()],
+                      newGDate3 = new Date(dataG[0].tarihGun3),
+                      newGdateMount3 = dd[newGDate3.getDay()],
+                      newGDate4 = new Date(dataG[0].tarihGun4),
+                      newGdateMount4 = dd[newGDate4.getDay()],
+                      newGDate5 = new Date(dataG[0].tarihGun5),
+                      newGdateMount5 = dd[newGDate5.getDay()];
+
+                    this.fiveDayWeather = [
+                      {
+                        degree: ((dataG[0].enDusukGun1 + dataG[0].enYuksekGun1) / 2).toFixed(0) + '°',
+                        icon: dataG[0].hadiseGun1,
+                        date: newGdateMount1
+                      },
+                      {
+                        degree: ((dataG[0].enDusukGun2 + dataG[0].enYuksekGun2) / 2).toFixed(0) + '°',
+                        icon: dataG[0].hadiseGun2,
+                        date: newGdateMount2
+                      },
+                      {
+                        degree: ((dataG[0].enDusukGun3 + dataG[0].enYuksekGun3) / 2).toFixed(0) + '°',
+                        icon: dataG[0].hadiseGun3,
+                        date: newGdateMount3
+                      },
+                      {
+                        degree: ((dataG[0].enDusukGun4 + dataG[0].enYuksekGun4) / 2).toFixed(0) + '°',
+                        icon: dataG[0].hadiseGun4,
+                        date: newGdateMount4
+                      },
+                      {
+                        degree: ((dataG[0].enDusukGun5 + dataG[0].enYuksekGun5) / 2).toFixed(0) + '°',
+                        icon: dataG[0].hadiseGun5,
+                        date: newGdateMount5
+                      }
+                    ]
+                  }catch(err) {
+                    alert(this.geoFindLocationDistrict + ' ilçesine ait 5 günlük veri bulunamamıştır.');
+                  }
+                  //console.log(this.fiveDayWeather);
+                });
+          })
+        },
+        fetchIlceId: function(e) {
+          fetch('https://servis.mgm.gov.tr/api/merkezler/ililcesi?il='+e)
+            .then(res => res.json())
+            .then(data => {
+              this.orderDistrict = data.sort((a,b) => (a.il > b.il) ? 1 : ((b.il > a.il) ? -1 : 0));
+              this.citiesDistrict = this.orderDistrict;
+            })
         }
     },
     beforeMount() {
@@ -284,6 +322,22 @@ export default {
         
         this.time = nowTime;
         this.getLocation();
+    },
+    computed: {
+      citiesFilter() {
+        var citiesFilters = this.cities.filter((item) => {
+          return item.il.toLowerCase().includes(this.cfilter.toLowerCase());
+        });
+
+        return citiesFilters;
+      },
+      citiesDistrictFilter() {
+        var citiesDistrictFilters = this.citiesDistrict.filter((item) => {
+          return item.ilce.toLowerCase().includes(this.dfilter.toLowerCase());
+        });
+
+        return citiesDistrictFilters;
+      }
     }
 }
 </script>
